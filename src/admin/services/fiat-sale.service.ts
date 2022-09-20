6dbe2af61d1e4a1839de4196e3fb10993e59939e
Trofimov Alex <overpod@yandex.ru>
2022-09-20 07:19:08 +0300

    refactoring: massCancelFiat

--- src/admin/services/fiat-sale.service.ts
+++ src/admin/services/fiat-sale.service.ts
@@ -1,18 +1,15 @@
-import { Injectable, Inject, HttpStatus, BadRequestException, Logger } from '@nestjs/common';
+import { Injectable, Inject, HttpStatus, BadRequestException } from '@nestjs/common';
 import { SignatureType, Account } from '@unique-nft/accounts';
 import { KeyringProvider } from '@unique-nft/accounts/keyring';
-import { Repository, DataSource } from 'typeorm';
-import { v4 as uuid } from 'uuid';
+import { Repository } from 'typeorm';
 import { Sdk } from '@unique-nft/substrate-client';
 import { KeyringPair } from '@polkadot/keyring/types';
 
 import { MarketConfig } from '@app/config/market-config';
-import { OffersEntity, BlockchainBlock, NFTTransfer } from '@app/entity';
+import { OffersEntity } from '@app/entity';
 import { ASK_STATUS } from '@app/escrow/constants';
 import { SellingMethod } from '@app/types';
-import { SearchIndexService } from '@app/auction/services/search-index.service';
 import { InjectUniqueSDK } from '@app/uniquesdk';
-import { InjectSentry, SentryService } from '@app/utils/sentry';
 import { PayOffersService } from '@app/offers/pay.service';
 import { OfferFiatDto } from '@app/offers/dto/pay.dto';
 
@@ -20,24 +17,14 @@ import { MassFiatSaleDTO, MassFiatSaleResultDto, MassCancelFiatResult } from '..
 
 @Injectable()
 export class FiatSaleService {
-  private logger: Logger;
   private auctionAccount: Account<KeyringPair>;
   private mainAccount: Account<KeyringPair>;
   private readonly offersRepository: Repository<OffersEntity>;
-  private readonly blockchainBlockRepository: Repository<BlockchainBlock>;
-  private readonly nftTransferRepository: Repository<NFTTransfer>;
   constructor(
-    private connection: DataSource,
     @Inject('CONFIG') private config: MarketConfig,
-    private readonly searchIndex: SearchIndexService,
     @InjectUniqueSDK() private readonly unique: Sdk,
-    @InjectSentry() private readonly sentryService: SentryService,
     private readonly payOffersService: PayOffersService,
   ) {
-    this.logger = new Logger(FiatSaleService.name);
-    this.offersRepository = this.connection.getRepository(OffersEntity);
-    this.blockchainBlockRepository = connection.getRepository(BlockchainBlock);
-    this.nftTransferRepository = connection.getRepository(NFTTransfer);
     this.auctionAccount = new KeyringProvider({ type: SignatureType.Sr25519 }).addSeed(this.config.auction.seed);
     this.mainAccount = new KeyringProvider({ type: SignatureType.Sr25519 }).addSeed(this.config.mainSaleSeed);
   }
@@ -109,64 +96,20 @@ export class FiatSaleService {
       },
     });
 
+    const canceledOfferFiat: OfferFiatDto[] = [];
+
     for (const offer of currentOffers) {
-      const { parsed, submittableResult, isCompleted } = await this.unique.tokens.transfer.submitWaitResult(
-        {
-          address: this.auctionAccount.instance.address,
-          to: this.mainAccount.instance.address,
-          collectionId: parseInt(offer.collection_id),
-          tokenId: parseInt(offer.token_id),
-        },
-        { signer: this.auctionAccount.getSigner() },
-      );
-
-      const blockHash = submittableResult.status.asInBlock;
-      const signedBlock = await this.unique.api.rpc.chain.getBlock(submittableResult.status.asInBlock);
-      const blockNumber = signedBlock.block.header.number.toBigInt();
-
-      if (blockNumber === undefined || blockNumber === null || blockNumber.toString() === '0') {
-        this.sentryService.message('sendTransferExtrinsic');
-
-        throw new BadRequestException({
-          statusCode: HttpStatus.CONFLICT,
-          message: 'Block number is not defined',
-        });
-      }
-
-      if (!isCompleted) {
-        this.sentryService.instance().captureException(new BadRequestException(submittableResult.internalError), {
-          tags: { section: 'fiat' },
-        });
-        throw new BadRequestException({
-          statusCode: HttpStatus.CONFLICT,
-          message: `Failed at block # ${blockNumber} (${blockHash.toHex()})`,
-        });
-      }
-      offer.block_number_cancel = blockNumber.toString();
-
-      this.logger.debug(`Token transfer block number: ${blockNumber}`);
-
-      const { collectionId, tokenId, from, to } = parsed;
-
-      const newTransfer = this.nftTransferRepository.create({
-        id: uuid(),
-        network: this.config.blockchain.unique.network,
-        collection_id: collectionId.toString(),
-        token_id: tokenId.toString(),
-        address_from: from,
-        address_to: to,
-        block_number: blockNumber.toString(),
-        created_at: new Date(),
+      const cancelOfferFiat = await this.payOffersService.cancelFiat({
+        collectionId: offer.collection_id,
+        tokenId: offer.token_id,
+        sellerAddress: offer.address_from,
       });
-
-      await this.nftTransferRepository.save(newTransfer);
+      canceledOfferFiat.push(cancelOfferFiat);
     }
-
-    const savedOffer = await this.offersRepository.save(currentOffers.map((offer) => ({ ...offer, status: ASK_STATUS.CANCELLED })));
 
     return {
       statusCode: HttpStatus.OK,
-      message: `${savedOffer.length} offers successfully canceled`,
+      message: `${canceledOfferFiat.length} offers successfully canceled`,
     };
   }
 }
