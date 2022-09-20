9165550be63dc533559f34f8e85c1509fa4001dc
Trofimov Alex <overpod@yandex.ru>
2022-09-20 06:34:49 +0300

    feactoring: massFiatSale

--- src/admin/services/fiat-sale.service.ts
+++ src/admin/services/fiat-sale.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject, HttpStatus, BadRequestException, Logger } from '@nestjs/common';
 import { SignatureType, Account } from '@unique-nft/accounts';
 import { KeyringProvider } from '@unique-nft/accounts/keyring';
-import { Repository, DataSource, In } from 'typeorm';
+import { Repository, DataSource } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 import { Sdk } from '@unique-nft/substrate-client';
 import { KeyringPair } from '@polkadot/keyring/types';
@@ -13,6 +13,8 @@ import { SellingMethod } from '@app/types';
 import { SearchIndexService } from '@app/auction/services/search-index.service';
 import { InjectUniqueSDK } from '@app/uniquesdk';
 import { InjectSentry, SentryService } from '@app/utils/sentry';
+import { PayOffersService } from '@app/offers/pay.service';
+import { OfferFiatDto } from '@app/offers/dto/pay.dto';
 
 import { MassFiatSaleDTO, MassFiatSaleResultDto, MassCancelFiatResult } from '../dto';
 
@@ -30,6 +32,7 @@ export class FiatSaleService {
     private readonly searchIndex: SearchIndexService,
     @InjectUniqueSDK() private readonly unique: Sdk,
     @InjectSentry() private readonly sentryService: SentryService,
+    private readonly payOffersService: PayOffersService,
   ) {
     this.logger = new Logger(FiatSaleService.name);
     this.offersRepository = this.connection.getRepository(OffersEntity);
@@ -55,102 +58,42 @@ export class FiatSaleService {
       ? data.tokenIds.filter((tokenId) => accountTokens.map(({ tokenId }) => tokenId).includes(tokenId)).map((token) => token.toString())
       : accountTokens.map(({ tokenId }) => tokenId.toString());
 
-    const tokenIds = tokenIdsAcount.length ? tokenIdsAcount : accountTokens.map(({ tokenId }) => tokenId.toString());
-
-    const newOffers: OffersEntity[] = tokenIds.map((tokenId) => {
-      return this.offersRepository.create({
-        id: uuid(),
-        type: SellingMethod.Fiat,
-        status: ASK_STATUS.ACTIVE,
-        collection_id: data.collectionId.toString(),
-        token_id: tokenId,
-        network: this.config.blockchain.unique.network,
-        price: (data.price * 100).toString(),
-        currency: data.currency,
-        address_from: this.mainAccount.instance.address,
-        address_to: this.auctionAccount.instance.address,
-      });
-    });
-
-    if (!newOffers.length) {
+    if (!tokenIdsAcount.length) {
       return {
         statusCode: HttpStatus.OK,
         message: 'Not tokens to sale',
-        data: [],
+        tokenIds: [],
       };
     }
 
-    for (const offer of newOffers) {
-      const { parsed, submittableResult, isCompleted } = await this.unique.tokens.transfer.submitWaitResult(
+    const offersFiat: OfferFiatDto[] = [];
+
+    for (const tokenId of tokenIdsAcount) {
+      const unsignedTxPayload = await this.unique.tokens.transfer.build(
         {
           address: this.mainAccount.instance.address,
           to: this.auctionAccount.instance.address,
-          collectionId: parseInt(offer.collection_id),
-          tokenId: parseInt(offer.token_id),
+          collectionId: data.collectionId,
+          tokenId: parseInt(tokenId),
         },
-        { signer: this.mainAccount.getSigner() },
+        { signer: this.mainAccount },
       );
 
-      const blockHash = submittableResult.status.asInBlock;
-      const signedBlock = await this.unique.api.rpc.chain.getBlock(submittableResult.status.asInBlock);
-      const blockNumber = signedBlock.block.header.number.toBigInt();
+      const { signature } = await this.mainAccount.sign(unsignedTxPayload);
 
-      if (blockNumber === undefined || blockNumber === null || blockNumber.toString() === '0') {
-        this.sentryService.message('sendTransferExtrinsic');
-
-        throw new BadRequestException({
-          statusCode: HttpStatus.CONFLICT,
-          message: 'Block number is not defined',
-        });
-      }
-
-      if (!isCompleted) {
-        this.sentryService.instance().captureException(new BadRequestException(submittableResult.internalError), {
-          tags: { section: 'fiat' },
-        });
-        throw new BadRequestException({
-          statusCode: HttpStatus.CONFLICT,
-          message: `Failed at block # ${blockNumber} (${blockHash.toHex()})`,
-        });
-      }
-
-      const block = this.blockchainBlockRepository.create({
-        network: this.config.blockchain.unique.network,
-        block_number: blockNumber.toString(),
-        created_at: new Date(),
+      const offerFiat = await this.payOffersService.createFiat({
+        price: data.price,
+        currency: data.currency,
+        signature,
+        signerPayloadJSON: unsignedTxPayload.signerPayloadJSON,
       });
-      await this.connection.createQueryBuilder().insert().into(BlockchainBlock).values(block).orIgnore().execute();
-
-      offer.block_number_ask = block.block_number;
-
-      this.logger.debug(`Token transfer block number: ${blockNumber}`);
-
-      const { collectionId, tokenId, from, to } = parsed;
-
-      const newTransfer = this.nftTransferRepository.create({
-        id: uuid(),
-        network: this.config.blockchain.unique.network,
-        collection_id: collectionId.toString(),
-        token_id: tokenId.toString(),
-        address_from: from,
-        address_to: to,
-        block_number: blockNumber.toString(),
-        created_at: new Date(),
-      });
-
-      await this.nftTransferRepository.save(newTransfer);
-    }
-
-    const saveOffers = await this.offersRepository.save(newOffers);
-
-    for (const item of saveOffers) {
-      await this.searchIndex.addSearchIndexIfNotExists({ collectionId: data.collectionId, tokenId: parseInt(item.token_id) });
+      offersFiat.push(offerFiat);
     }
 
     return {
       statusCode: HttpStatus.OK,
       message: 'Mass fiat listing completed',
-      data: saveOffers.map(({ token_id }) => parseInt(token_id)),
+      tokenIds: offersFiat.map((offer) => offer.tokenId),
     };
   }
 
