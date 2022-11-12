67cef82722e463d0f83a107893a593f535357dcf
SergeyOkishev <sokishev@usetech.com>
2022-11-12 13:37:23 +0300

    Cleared copiesCount migration body.

--- src/migrations/1668054011132-lazy_pay_token.ts
+++ src/migrations/1668054011132-lazy_pay_token.ts
@@ -2,10 +2,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class lazyPayToken1668054011132 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE public.offers ADD "copiesCount" int8 NULL`);
+    // await queryRunner.query(`ALTER TABLE public.offers ADD "copiesCount" int8 NULL`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE public.offers DROP COLUMN "copiesCount"`);
+    // await queryRunner.query(`ALTER TABLE public.offers DROP COLUMN "copiesCount"`);
   }
 }
