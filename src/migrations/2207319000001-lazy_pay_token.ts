47891a72699fd0043c33fe51bdc9db05a7973a42
Trofimov Alex <overpod@yandex.ru>
2022-11-12 14:22:23 +0300

    fix: Migrations

--- src/migrations/2207319000001-lazy_pay_token.ts
+++ src/migrations/2207319000001-lazy_pay_token.ts
@@ -4,10 +4,10 @@ export class LazyPayToken_2207319000001 implements MigrationInterface {
 
   name = 'LazyPayToken_2207319000001';
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // await queryRunner.query(`ALTER TABLE public.offers ADD "copiesCount" int8 NULL`);
+    await queryRunner.query(`ALTER TABLE public.offers ADD "copiesCount" int8 NULL`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    // await queryRunner.query(`ALTER TABLE public.offers DROP COLUMN "copiesCount"`);
+    await queryRunner.query(`ALTER TABLE public.offers DROP COLUMN "copiesCount"`);
   }
 }
